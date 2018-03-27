dad2b86a31dabc1824b1fdbd7517ca440bd0c751
a365ngay <35994656+a365ngay@users.noreply.github.com>
2018-03-27 15:19:11 +0700

    update thêm sv, pm

---
+++ QLTV/QLTV/GUI/frmAddpm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BUS;
+
+namespace QLTV.GUI
+{
+    public partial class frmAddpm : Form
+    {
+        Bus db = new Bus();
+        public frmAddpm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            int x = db.add_PhieuMuon(txtMapm.Text, txtManv.Text, txtMasv.Text, dtpNgaytao.Text);
+            if (x == 1)
+            {
+                MessageBox.Show("Thêm thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.None);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Kiểm tra lại mã phiếu mượn, mã nhân viên và mã sinh viên!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ QLTV/QLTV/GUI/frmAddpm.designer.cs
@@ -0,0 +1,179 @@
+﻿namespace QLTV.GUI
+{
+    partial class frmAddpm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtpNgaytao = new System.Windows.Forms.DateTimePicker();
+            this.txtManv = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMapm = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMasv = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(408, 222);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(256, 222);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 7;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtpNgaytao);
+            this.groupBox1.Controls.Add(this.txtManv);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtMapm);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtMasv);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(20, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(506, 172);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin phiếu mượn";
+            // 
+            // dtpNgaytao
+            // 
+            this.dtpNgaytao.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNgaytao.Location = new System.Drawing.Point(363, 102);
+            this.dtpNgaytao.Name = "dtpNgaytao";
+            this.dtpNgaytao.Size = new System.Drawing.Size(100, 20);
+            this.dtpNgaytao.TabIndex = 2;
+            // 
+            // txtManv
+            // 
+            this.txtManv.Location = new System.Drawing.Point(108, 102);
+            this.txtManv.Name = "txtManv";
+            this.txtManv.Size = new System.Drawing.Size(100, 20);
+            this.txtManv.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(282, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Ngày tạo";
+            // 
+            // txtMapm
+            // 
+            this.txtMapm.Location = new System.Drawing.Point(363, 38);
+            this.txtMapm.Name = "txtMapm";
+            this.txtMapm.Size = new System.Drawing.Size(100, 20);
+            this.txtMapm.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Mã nhân viên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Mã phiếu mượn";
+            // 
+            // txtMasv
+            // 
+            this.txtMasv.Location = new System.Drawing.Point(108, 38);
+            this.txtMasv.Name = "txtMasv";
+            this.txtMasv.Size = new System.Drawing.Size(100, 20);
+            this.txtMasv.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã sinh viên";
+            // 
+            // frmAddpm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 262);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmAddpm";
+            this.Text = "Thêm phiếu mượn";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtpNgaytao;
+        private System.Windows.Forms.TextBox txtManv;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMapm;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMasv;
+        private System.Windows.Forms.Label label1;
+    }
+}
